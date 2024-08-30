27b255310c9d3220441d29fea5b4feb5957502df
sonup <Sonup5018@gmail.com>
2024-08-30 11:29:33 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
 import Header from './assests/components/Header'
+import Footer from './assests/components/Footer/Footer.jsx'
+
 function App() {
   return (
     <div className='App'>
     <Header/>
     <div className='bg-indigo-600 w-full h-screen'></div>
+    <Footer/>
     </div>
   );
 }
